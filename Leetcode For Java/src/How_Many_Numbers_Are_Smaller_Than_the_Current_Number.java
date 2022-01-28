7cd3e4719985c2698ab865b9179ff817b82a4514
Sma1lboy <chen.10724@buckeyemail.osu.edu>
2022-01-28 10:54:27 -0500

    src code

--- Leetcode For Java/src/How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java
+++ Leetcode For Java/src/How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class How_Many_Numbers_Are_Smaller_Than_the_Current_Number {
+import java.util.Arrays;
+
+public class How_Many_Numbers_Are_Smaller_Than_the_Current_Number {
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] result = new int[nums.length];
+        int j = 0;
+        for (int num : nums) {
+            int numSmaller = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (num > nums[i]) {
+                    numSmaller++;
+                }
+            }
+            result[j] = numSmaller;
+            j++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] num = {8,1,2,2,3};
+        int[] result = smallerNumbersThanCurrent(num);
+        System.out.println(Arrays.toString(result));
+    }
 }
--- Leetcode For Java/src/Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.java
+++ Leetcode For Java/src/Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Subtract_the_Product_and_Sum_of_Digits_of_an_Integer {
+//1281n 
+public class Subtract_the_Product_and_Sum_of_Digits_of_an_Integer {
+
+    public int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+
+        while (n != 0) {
+//          FIXME 243
+            int rem = n % 10;
+            product *= rem;
+            sum += rem;
+            n /= 10;
+        }
+
+        return product - sum;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
