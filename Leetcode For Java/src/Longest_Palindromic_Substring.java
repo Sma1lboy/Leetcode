1077df0aa39dc0b67711c7a53c63b88e7ee1cff6
Sma1lboy <chen.10724@buckeyemail.osu.edu>
2022-04-22 02:05:40 -0400

    src

--- Leetcode For Java/src/Longest_Palindromic_Substring.java
+++ Leetcode For Java/src/Longest_Palindromic_Substring.java
@@ -4,28 +4,24 @@ import java.util.Map;
 //5
 public class Longest_Palindromic_Substring {
     public static String longestPalindrome(String s) {
-        int longestLeng = 0;
-        HashMap<Integer, int[]> result = new HashMap<>();
+        int longestLength = 0;
+        String ans = "";
         for (int i = 0; i < s.length(); i++) {
-            for (int j = 1; j < s.length(); j++) {
-                if (s.charAt(i) == s.charAt(j)) {
-                    result.put(j - i, new int[]{i, j});
+            for (int j = i; j < s.length(); j++) {
+                if(s.charAt(i) == s.charAt(j)) {
+                    if(j - i > longestLength) {
+                        longestLength = j - i;
+                        ans = s.substring(i, j + 1);
+                        break;
+                    }
                 }
             }
         }
-        for (int[] num : result.values()) {
-            int temp = num[1] - num[0];
-            if (temp > longestLeng) {
-                longestLeng = temp;
-            }
-        }
-        int[] numArr = result.get(longestLeng);
-
-        return s.substring(numArr[0], numArr[1] + 1);
+        return ans;
     }
 
     public static void main(String[] args) {
-        String s = "cbbd";
+        String s = "a";
         System.out.println(longestPalindrome(s));
     }
 }
