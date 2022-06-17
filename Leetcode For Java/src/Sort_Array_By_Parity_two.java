f72d392bf1c652c2c120a38d701eb11e6058dec9
BuildTools <541898146chen@gmail.com>
2022-06-17 12:07:47 -0400

    ^^

--- Leetcode For Java/src/Sort_Array_By_Parity_two.java
+++ Leetcode For Java/src/Sort_Array_By_Parity_two.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Sort_Array_By_Parity_two {
+public class Sort_Array_By_Parity_two {
+    public int[] sortArrayByParityII(int[] nums) {
+        int n = nums.length;
+        int l = 0, r = n - 1;
+        while(l < n &&  r >= 0) {
+            if(nums[l] % 2 == 0) {
+                l += 2;
+            } else if (nums[r] % 2 == 1) {
+                r -= 2;
+            } else {
+                swap(nums, l, r );
+            }
+        }
+        return nums;
+    }
+    private void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
 }
