dbcd7cb70cf3242ea22d22274d58a73fff1f9360
BuildTools <541898146chen@gmail.com>
2022-06-17 12:56:21 -0400

    ^^^^^^

---
+++ Leetcode For Java/src/Is_Subsequence.java
@@ -0,0 +1,14 @@
+public class Is_Subsequence {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        while(i < s.length() && j < t.length()) {
+            if(s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return i == s.length();
+    }
+}
