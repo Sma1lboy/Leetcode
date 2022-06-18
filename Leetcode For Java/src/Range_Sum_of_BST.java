997fa89e376f602333afe3a9118bd6316239d05b
BuildTools <541898146chen@gmail.com>
2022-06-18 17:40:44 -0400

    ^^^^^^

---
+++ Leetcode For Java/src/Range_Sum_of_BST.java
@@ -0,0 +1,10 @@
+public class Range_Sum_of_BST {
+//    public int rangeSumBST(TreeNode root, int low, int high) {
+//        if(root == null) return 0;
+//        int res = 0;
+//        if(root.val >= low && root.val <= high) res += root.val;
+//        res += rangeSumBST(root.left, low, high);
+//        res += rangeSumBST(root.right , low, high);
+//        return res;
+//    }
+}
