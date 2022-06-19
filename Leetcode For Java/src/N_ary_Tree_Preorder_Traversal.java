4d7c72c854276e20c361a1bc85c93a53ae53d948
BuildTools <541898146chen@gmail.com>
2022-06-18 21:34:35 -0400

    ^A*S&D

---
+++ Leetcode For Java/src/N_ary_Tree_Preorder_Traversal.java
@@ -0,0 +1,17 @@
+public class N_ary_Tree_Preorder_Traversal {
+    /**
+     *  DFS case:
+     */
+//    public List<Integer> preorder(Node root) {
+//        List<Integer> list = new ArrayList<>();
+//        dfsHelper(root, list);
+//        return list;
+//    }
+//    public void dfsHelper(Node root, List<Integer> list) {
+//        if (root == null) return;
+//        list.add(root.val);
+//        for(Node child : root.children) {
+//            dfsHelper(child, list);
+//        }
+//    }
+}
---
+++ Leetcode For Java/src/Sum_of_Root_To_Leaf_Binary_Numbers.java
@@ -0,0 +1,17 @@
+public class Sum_of_Root_To_Leaf_Binary_Numbers {
+    public int sumRootToLeaf(TreeNode root) {
+//        return dfs(root, 0);
+//
+//    }
+//    public int dfs(TreeNode root, int sum) {
+//        if(root == null) return 0;
+//
+//        sum = (sum<<1) + root.val;
+//
+//        if(root.left == null && root.right == null) {
+//            return sum;
+//        }
+//
+//        return dfs(root.left, sum) + dfs(root.right, sum);
+//    }
+}
