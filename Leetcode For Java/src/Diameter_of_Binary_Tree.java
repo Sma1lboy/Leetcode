fdadb7413a761254dac166decffad21f4fb3d7ac
Sma1lboy <541898146chen@gmail.com>
2022-06-22 18:29:24 -0400

    add some questions

---
+++ Leetcode For Java/src/Diameter_of_Binary_Tree.java
@@ -0,0 +1,14 @@
+public class Diameter_of_Binary_Tree {
+//    int max = 0;
+//    public int diameterOfBinaryTree(TreeNode root) {
+//        maxDepth(root);
+//        return max;
+//    }
+//    public int maxDepth(TreeNode root) {
+//        if(root == null) return 0;
+//        int left = maxDepth(root.left);
+//        int right = maxDepth(root.right);
+//        max = Math.max(max, left + right);
+//        return Math.max(left, right) + 1;
+//    }
+}
