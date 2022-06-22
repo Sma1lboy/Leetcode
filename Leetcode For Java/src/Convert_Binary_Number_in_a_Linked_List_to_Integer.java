3234da45dedec504778228c0681921c378a6db82
Sma1lboy <541898146chen@gmail.com>
2022-06-22 00:20:42 -0400

    s

---
+++ Leetcode For Java/src/Convert_Binary_Number_in_a_Linked_List_to_Integer.java
@@ -0,0 +1,13 @@
+public class Convert_Binary_Number_in_a_Linked_List_to_Integer {
+//    class Solution {
+//        public int getDecimalValue(ListNode head) {
+//            if(head.next == null) {
+//                return head.val;
+//            } else {
+//                head.next.val += head.val << 1;
+//                return getDecimalValue(head.next);
+//            }
+//
+//        }
+//    }
+}
