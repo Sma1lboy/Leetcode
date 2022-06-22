1350f299db589f22b058e9cc9ff1b393627b8bbb
Sma1lboy <541898146chen@gmail.com>
2022-06-22 13:16:36 -0400

    add more code

---
+++ Leetcode For Java/src/Leaf_Similar_Trees.java
@@ -0,0 +1,15 @@
+public class Leaf_Similar_Trees {
+//    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+//        List<Integer> list1 = new ArrayList<>();
+//        List<Integer> list2 = new ArrayList<>();
+//        dfs(root1, list1);
+//        dfs(root2, list2);
+//        return list1.equals(list2);
+//    }
+//    public void dfs(TreeNode root, List<Integer> list) {
+//        if(root == null) return;
+//        if(root.left == null && root.right == null) list.add(root.val);
+//        dfs(root.left, list);
+//        dfs(root.right, list);
+//    }
+}
