3f043173a28b40747e32126e530f20174078daad
Sma1lboy <541898146chen@gmail.com>
2022-06-29 23:31:44 -0400

    minimum moves one

--- Leetcode For Java/src/Minimum_Moves_to_Equal_Array_Elements.java
+++ Leetcode For Java/src/Minimum_Moves_to_Equal_Array_Elements.java
@@ -1,23 +1,20 @@
 import java.util.Arrays;
 
 public class Minimum_Moves_to_Equal_Array_Elements {
-    public static int minMoves2(int[] nums) {
-        //length
-        int len = nums.length;
-        //sort first
+
+    /**
+     * sdsd
+     * @param nums
+     * @return
+     * <img src="/Users/jacksonchen/Desktop/Screen Shot 2022-06-29 at 11.24.50 PM.png" />
+     */
+    public int minMoves(int[] nums) {
         Arrays.sort(nums);
-        //find the median number
-        int median = nums[len/2];
-        int res = 0;
-        for(int num : nums) {
-            res += Math.abs(num-median);
+        int sum = 0;
+        int min = nums[0];
+        for(int num:nums){
+            sum+=num;
         }
-        return res;
-
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {1,10,2,9};
-        System.out.println(minMoves2(arr));
+        return sum - nums.length * min;
     }
 }
---
+++ Leetcode For Java/src/Minimum_Moves_to_Equal_Array_Elements_two.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Minimum_Moves_to_Equal_Array_Elements_two {
+    public static int minMoves2(int[] nums) {
+        //length
+        int len = nums.length;
+        //sort first
+        Arrays.sort(nums);
+        //find the median number
+        int median = nums[len/2];
+        int res = 0;
+        for(int num : nums) {
+            res += Math.abs(num-median);
+        }
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,10,2,9};
+        System.out.println(minMoves2(arr));
+    }
+}
