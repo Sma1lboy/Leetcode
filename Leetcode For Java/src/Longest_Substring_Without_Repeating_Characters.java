6a9750a9b69f59e0d890e943bce2d814195c5f89
Sma1lboy <541898146chen@gmail.com>
2022-07-17 22:25:19 -0400

    update

--- Leetcode For Java/src/Longest_Substring_Without_Repeating_Characters.java
+++ Leetcode For Java/src/Longest_Substring_Without_Repeating_Characters.java
@@ -6,21 +6,47 @@ public class Longest_Substring_Without_Repeating_Characters {
         System.out.println(lengthOfLongestSubstring(""));
     }
 
-    public static int lengthOfLongestSubstring(String s) {
-        int start = 0, end = 0, maxSize = 0;
-        HashSet<Character> hs = new HashSet<>();
-        while (end <s.length()) {
-            if (!hs.contains(s.charAt(end))) {
-                hs.add(s.charAt(end));
-                end++;
-                maxSize = Math.max(maxSize, hs.size());
-            }
-            else {
-                hs.remove(s.charAt(start));
-                start++;
-            }
+//    public static int lengthOfLongestSubstring(String s) {
+//        int start = 0, end = 0, maxSize = 0;
+//        HashSet<Character> hs = new HashSet<>();
+//        while (end <s.length()) {
+//            if (!hs.contains(s.charAt(end))) {
+//                hs.add(s.charAt(end));
+//                end++;
+//                maxSize = Math.max(maxSize, hs.size());
+//            }
+//            else {
+//                hs.remove(s.charAt(start));
+//                start++;
+//            }
+//        }
+//        return maxSize;
+//    }
+public static int lengthOfLongestSubstring(String s) {
+    //freq map
+    int[] map = new int[128];
+    int start = 0, end = 0, counter = 0;
+    int maxLen = 0;
+    while(end < s.length()) {
+        char c1 = s.charAt(end);
+        //if no character exist in curr position
+        if(map[c1] > 0) {
+            counter++;
         }
-        return maxSize;
+        map[c1]++;
+        end++;
+        //when the condition met
+        while(counter > 0) {
+            char c2 = s.charAt(start);
+            if(map[c2] > 1) {
+                counter--;
+            }
+            map[c2]--;
+            start++;
+        }
+        maxLen = Math.max(maxLen, end - start);
     }
+    return maxLen;
+}
 
 }
